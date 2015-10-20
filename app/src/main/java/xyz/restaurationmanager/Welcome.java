e2913a8a686607cb53b779ebf0d0d07977c08402
Yoann Moise <moise.yoann@gmail.com>
2015-10-20 21:00:31 +0200

    classes and interfaces

--- app/src/main/java/xyz/restaurationmanager/Welcome.java
+++ app/src/main/java/xyz/restaurationmanager/Welcome.java
@@ -1,20 +1,18 @@
 package xyz.restaurationmanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_welcome);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,12 +24,6 @@ public class RegisterActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Button btn = (Button) findViewById(R.id.buttonEnregistrerInscription);
-
     }
-
-
 
 }
