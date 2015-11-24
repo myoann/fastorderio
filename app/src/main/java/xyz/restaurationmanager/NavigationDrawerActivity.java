a2495da06b9bae95e519f3a7ab30889b7af44ebc
Yoann <moise.yoann@gmail.com>
2015-11-24 15:39:27 +0100

    New Menu

--- app/src/main/java/xyz/restaurationmanager/NavigationDrawerActivity.java
+++ app/src/main/java/xyz/restaurationmanager/NavigationDrawerActivity.java
@@ -96,10 +96,6 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
         } else if (id == R.id.nav_send) {
 
         }
