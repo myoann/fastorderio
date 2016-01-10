9a91fd027aacd4db80044aec6f19074a28e6e3cc
Yoann Moise <moise.yoann@gmail.com>
2016-01-11 00:00:00 +0100

    Nouvelle version

--- app/src/main/java/xyz/restaurationmanager/AddMenuActivity.java
+++ app/src/main/java/xyz/restaurationmanager/AddMenuActivity.java
@@ -13,6 +13,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxCallback;
@@ -25,6 +26,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -43,12 +45,57 @@ public class AddMenuActivity extends AppCompatActivity implements View.OnClickLi
 
     AQuery aq;
     String url = "http://92.243.14.22:1337/menu/";
+    TextView eTextNbArticles;
+    TextView eTextReduc;
+    TextView eTextPrixTotal;
+    TextView tEntrees;
+    TextView tPlats;
+    TextView tDesserts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("On create AddMenuActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_menu);
+        final List<Product> cart = ListProduct.getCart();
+
+        this.eTextNbArticles = (TextView)(this.findViewById(R.id.nbArticles));
+        this.eTextNbArticles.setText(String.valueOf(cart.size()));
+
+        Double reducTotal = 0.0;
+        Double prixTotal = 0.0;
+        Double prixTotalReduit = 0.0;
+        int nbEntrees = 0;
+        int nbPlats = 0;
+        int nbDesserts = 0;
+
+        for(int i = 0; i<cart.size();i++) {
+            if ((cart.get(i).getType() == "Entrée") || (cart.get(i).getType() == "Appéritif")) {
+                nbEntrees += 1;
+            } else if ((cart.get(i).getType() == "Plat") || (cart.get(i).getType() == "Plat ")) {
+                nbPlats += 1;
+            } else if (cart.get(i).getType() == "Dessert") {
+                nbDesserts += 1;
+            }
+            reducTotal += cart.get(i).getDiscount();
+            prixTotal += cart.get(i).getPrice();
+        }
+        prixTotalReduit = prixTotal - reducTotal;
+
+        this.eTextReduc = (TextView)(this.findViewById(R.id.reduction));
+        this.eTextReduc.setText(String.valueOf(reducTotal) + " €");
+
+        this.eTextPrixTotal = (TextView)(this.findViewById(R.id.prixtotal));
+        this.eTextPrixTotal.setText(String.valueOf(prixTotalReduit) + " €");
+
+        this.tEntrees = (TextView)(this.findViewById(R.id.entrees));
+        this.tEntrees.setText(nbEntrees + " Entrées & Aperos");
+
+        this.tPlats = (TextView)(this.findViewById(R.id.plats));
+        this.tPlats.setText(nbPlats + " Plats");
+
+        this.tDesserts = (TextView)(this.findViewById(R.id.desserts));
+        this.tDesserts.setText(nbDesserts + " Desserts");
 
         // Set up the register form.
         //mEmailView = (AutoCompleteTextView) findViewById(R.id.editTextRegisterEmail);
