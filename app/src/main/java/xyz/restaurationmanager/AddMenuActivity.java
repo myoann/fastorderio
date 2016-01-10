7dd92aba9295fdc997901c901c0ee37c3e40e115
Florian <florian.m.06@gmail.com>
2016-01-10 23:54:36 +0100

    envoie de commande

--- app/src/main/java/xyz/restaurationmanager/AddMenuActivity.java
+++ app/src/main/java/xyz/restaurationmanager/AddMenuActivity.java
@@ -63,46 +63,9 @@ public class AddMenuActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        //String price = mEmailView.getText().toString();
-        Double price = 302.0;
-        Double discount = 37.5;
-        String server = "7531";
-        String cooker = "642";
-        ArrayList<String> items = new ArrayList<String>();
-       /* items.add("564113af6c7e6511333f616c");
-        items.add("564113af6c7e6511333f6172");
-        items.add("564113af6c7e6511333f6165");*/
-
         aq = new AQuery(v);
-
         boolean cancel = false;
         View focusView = null;
-    /*
-        // Check for not null server
-        if (TextUtils.isEmpty(server)) {
-            //mFirstNameView.setError(getString(R.string.error_field_required));
-            //focusView = mFirstNameView;
-            cancel = true;
-        }
-        // Check for not null cooker
-        if (TextUtils.isEmpty(cooker)) {
-            //mLastNameView.setError(getString(R.string.error_field_required));
-            //focusView = mLastNameView;
-            cancel = true;
-        }
-
-        // Check for items to exist
-        if (items.size() == 0) {
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {*/
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
 
         JSONObject json = new JSONObject();
 
@@ -131,72 +94,6 @@ public class AddMenuActivity extends AppCompatActivity implements View.OnClickLi
 
             }
         });
-           /* Map<String, Object> params = new HashMap<String, Object>();
-            params.put("price", 322);
-            params.put("discount", 12);
-            params.put("server",server);
-            params.put("cooker",cooker);
 
-            JSONArray itemsArray = new JSONArray();
-            JSONObject anItem = new JSONObject();
-         //   anItem.put("id", (String) " okokook");
-
-
-            //JSONArray itemss = new JSONArray()
-           /* HashMap<String, Object> itemss = new HashMap<String, Object>();
-            for(int i = 0; i<items.size();i++) {
-                try {
-                    itemsArray.put(i, anItem);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-            params.put("items", itemsArray);
-
-
-            //params.put("items", itemss);
-
-            JSONObject input = new JSONObject();
-            try {
-                input.put("price", 30);
-                input.put("discount", 1);
-                JSONObject serverO = new JSONObject();
-                serverO.put("id", "569147e502399fd203f3dee0");
-                input.put("server", serverO);
-                JSONObject cookerO = new JSONObject();
-                cookerO.put("id", "56926330e0cac4380e759bd9");
-                input.put("cooker", cookerO);
-
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Log.d("input",input.toString());
-        aq.post(url, input, JSONArray.class, new AjaxCallback<JSONArray>() {
-            public void callback(String url, JSONArray menu, AjaxStatus status) {
-                Log.d("AGSON Object:", menu.toString());
-            }
-        });*/
-/*
-            aq = new AQuery(v);
-            aq.transformer(t).ajax(url, params, Menu.class, new AjaxCallback<Menu>() {
-                public void callback(String url, Menu menu, AjaxStatus status) {
-                    Gson gson = new Gson();
-                    Log.d("GSON status", status.getMessage() + status.getError());
-                    Log.d("GSON Object:", gson.toJson(menu));
-                }
-            });
-            //startActivity(new Intent(AddMenuActivity.this, NavigationDrawerActivity.class));
-
-
-            /*aq.post(url, obj, JSONObject.class, new AjaxCallback<JSONObject>() {
-                public void callback(String url, JSONObject json, AjaxStatus status) {
-                    Log.d("Réponse", "" + json);
-                }
-            });*/
-
-        //}
     }
 }
